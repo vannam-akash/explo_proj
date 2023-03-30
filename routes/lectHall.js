36793d35ba99a39d68cab0bc9505a8c8d7acb199
octospaien <user3.insti@gmail.com>
2023-03-31 03:10:44 +0530

    This commit implements feature to ensure atmost 1 Hall bokking per professor.

--- routes/lectHall.js
+++ routes/lectHall.js
@@ -1,29 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const app = express();
 
 const Professor = require('../models/professor');
 const LectureHall = require('../models/lectHall');
 
 router.route('/')
-    .get(async(req,res)=>{
+    .get(async(req,res)=>{  
         const lts = await LectureHall.find({});
-        // console.log(req.session);
-        const profID = req.session.profId || null;
-        res.render('lectHalls/index',{lts,profID});
+        //console.log(req.session);
+        let profID = req.session.profId || null;
+        if(profID)profID=new mongoose.Types.ObjectId(profID);
+        console.log(profID);
+        let prof_=null;
+        if(profID) {prof_ = await Professor.findById(profID);}
+        console.log("start",prof_,"end");
+        res.render('lectHalls/index',{lts,prof_});
     });
+
+
 
 router.route('/:lid/prof/:profid')
     .put(async(req,res)=>{
         const {lid,profid} = req.params;
-        const prof = await Professor.findById(profid);
+        //profid=new mongoose.Types.ObjectId(profid);
+        const prof_ = await Professor.findById(profid);
+        //console.log(lid);
         const lt = await LectureHall.findById(lid);
-        lt.occupiedBy = prof.name;
-        lt.class = prof.class;
+        lt.occupiedBy = prof_.name;
+        lt.class = prof_.class;
         lt.status = "Occupied";
         await lt.save();
-        const lts = await LectureHall.find();
-        res.render('lectHalls/index',{lts,profID:profid});
+        
+        console.log("start",prof_,"end");
+        res.redirect(`/lectHalls/prof/${profid}`);
     });
+
+router.route('/prof/:profid')
+    .get(async(req,res)=>{
+        const {profid} = req.params;
+        //profid=new mongoose.Types.ObjectId(profid);
+        const prof_ = await Professor.findById(profid);
+        const lts = await LectureHall.find({});
+        res.render('lectHalls/index',{lts,prof_});
+    });
+
+
+
+
+
 
 module.exports = router;
